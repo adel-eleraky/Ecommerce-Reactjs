5c66f46d83ca190e0b7b8d453c6a13552b4159e5
el3raky <kameladel339@gmail.com>
2024-01-12 20:56:28 +0200

    finished login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,116 +1,40 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import "./css/Login.css"
+import { Link } from 'react-router-dom'
 
 function Login() {
     return (
         <>
-            <div className="container">
-                <div className="row">
-                        <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-                            <li className="nav-item" role="presentation">
-                                <a className="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
-                                    aria-controls="pills-login" aria-selected="true">Login</a>
-                            </li>
-                            <li className="nav-item" role="presentation">
-                                <a className="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
-                                    aria-controls="pills-register" aria-selected="false">Register</a>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                            <div className="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-                                <form>
-                                    <div className="text-center mb-3">
-                                        <p>Sign in with:</p>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-facebook-f"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-google"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-twitter"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-github"></i>
-                                        </button>
+            <div className="login-page py-5">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-sm-10 col-md-8 col-lg-7">
+                            <div className="content  bg-white p-4 rounded">
+                                <h3 className='mb-4 text-center'>Login in to Your Account</h3>
+                                <form action="" className='login-form mb-3'>
+                                    <div className="form-floating mb-3">
+                                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                        <label htmlFor="floatingInput">Email address</label>
                                     </div>
-                                    <p className="text-center">or:</p>
-                                    <div className="form-outline mb-4">
-                                        <input type="email" id="loginName" className="form-control" />
-                                        <label className="form-label" htmlFor="loginName">Email or username</label>
+                                    <div className="form-floating mb-3">
+                                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                                        <label htmlFor="floatingPassword">Password</label>
                                     </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="password" id="loginPassword" className="form-control" />
-                                        <label className="form-label" htmlFor="loginPassword">Password</label>
-                                    </div>
-                                    <div className="row mb-4">
-                                        <div className="col-md-6 d-flex justify-content-center">
-                                            <div className="form-check mb-3 mb-md-0">
-                                                <input className="form-check-input" type="checkbox" value="" id="loginCheck"  />
-                                                <label className="form-check-label" htmlFor="loginCheck"> Remember me </label>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6 d-flex justify-content-center">
-                                            <a href="#!">Forgot password?</a>
-                                        </div>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
-                                    <div className="text-center">
-                                        <p>Not a member? <a href="#!">Register</a></p>
-                                    </div>
+                                    <Link to="/forget-password" className='text-dark fs-6 fw-bold '  >forget password ?</Link>
+                                    <button className='btn d-block mx-auto text-white mb-5 mt-2'>Log In</button>
                                 </form>
-                            </div>
-                            <div className="tab-pane fade " id="pills-register" role="tabpanel" aria-labelledby="tab-register">
-                                <form>
-                                    <div className="text-center mb-3">
-                                        <p>Sign up with:</p>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-facebook-f"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-google"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-twitter"></i>
-                                        </button>
-                                        <button type="button" className="btn btn-link btn-floating mx-1">
-                                            <i className="fab fa-github"></i>
-                                        </button>
-                                    </div>
-                                    <p className="text-center">or:</p>
-                                    <div className="form-outline mb-4">
-                                        <input type="text" id="registerName" className="form-control" />
-                                        <label className="form-label" htmlFor="registerName">Name</label>
-                                    </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="text" id="registerUsername" className="form-control" />
-                                        <label className="form-label" htmlFor="registerUsername">Username</label>
-                                    </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="email" id="registerEmail" className="form-control" />
-                                        <label className="form-label" htmlFor="registerEmail">Email</label>
-                                    </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="password" id="registerPassword" className="form-control" />
-                                        <label className="form-label" htmlFor="registerPassword">Password</label>
-                                    </div>
-                                    <div className="form-outline mb-4">
-                                        <input type="password" id="registerRepeatPassword" className="form-control" />
-                                        <label className="form-label" htmlFor="registerRepeatPassword">Repeat password</label>
-                                    </div>
-                                    <div className="form-check d-flex justify-content-center mb-4">
-                                        <input className="form-check-input me-2" type="checkbox" value="" id="registerCheck" 
-                                            aria-describedby="registerCheckHelpText" />
-                                        <label className="form-check-label" htmlFor="registerCheck">
-                                            I have read and agree to the terms
-                                        </label>
-                                    </div>
-                                    <button type="submit" className="btn btn-primary btn-block mb-3">Sign in</button>
-                                </form>
+                                <h6 className='text-center mb-3'>Login With Social Media Account</h6>
+                                <div className="social-icons d-flex justify-content-center gap-15 mb-4">
+                                    <div className="facebook-icon"><i className="fa-brands fa-facebook fs-1"></i></div>
+                                    <div className="google-icon"><i className="fa-brands fa-google fs-1"></i></div>
+                                </div>
+                                <h6 className='text-center mb-3'>Don't Have Account ? </h6>
+                                <Link to="/sign-up" ><button className='btn btn-secondary d-block mx-auto'>Sign Up</button> </Link>
                             </div>
                         </div>
-                    
+                    </div>
                 </div>
             </div>
         </>
