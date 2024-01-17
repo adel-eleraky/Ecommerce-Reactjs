fa7468179364064fabbe4dba4974ea83a3e776de
el3raky <kameladel339@gmail.com>
2024-01-17 18:10:10 +0200

    added product desc & add-to-cart button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,30 +7,35 @@ import { Link } from "react-router-dom"
 function ProductCard() {
     return (
         <>
-                <div className="featured-product border p-3 mb-3 position-relative rounded" data-aos="fade-up" data-aos-duration="1000">
-                    <img src="/images/wish.svg" className='position-absolute wish-icon' />
-                    <div className="icons position-absolute">
-                        <img src="/images/prodcompare.svg" className='d-block mb-3' />
-                        <img src="/images/view.svg" className='d-block mb-3' />
-                        <img src="/images/img-zoom.svg" className='d-block mb-3' />
-                        <img src="/images/add-cart.svg" className='d-block mb-3' />
-                    </div>
-                    <img className='img-fluid' src="/images/tab.jpg" alt="product img" />
-                    <div className="product-content">
-                        <div className="product-brand mb-3">Havells</div>
-                        <Link to="products/portable-speaker" className='text-dark'>
-                            <div className="product-name fw-bold mb-3">Honor T1 7.0 1GB RAM 8GB ROM</div>
-                        </Link>
-                        <ReactStars
-                            count={5}
-                            size={24}
-                            value={3}
-                            edit={false}
-                            activeColor="#ffd700"
-                        />
-                        <div className="price">Price: $100</div>
+            <div className="featured-product border p-3 mb-3 position-relative rounded" data-aos="fade-up" data-aos-duration="1000">
+                <img src="/images/wish.svg" className='position-absolute wish-icon' />
+                <div className="icons position-absolute">
+                    <img src="/images/prodcompare.svg" className='d-block mb-3' />
+                    <img src="/images/view.svg" className='d-block mb-3' />
+                    <img src="/images/img-zoom.svg" className='d-block mb-3' />
+                    <img src="/images/add-cart.svg" className='d-block mb-3' />
+                </div>
+                <img className='img-fluid' src="/images/tab.jpg" alt="product img" />
+                <div className="product-content">
+                    <div className="product-brand mb-3">Havells</div>
+                    <Link to="products/portable-speaker" className='text-dark'>
+                        <div className="product-name fw-bold mb-3">Honor T1 7.0 1GB RAM 8GB ROM</div>
+                    </Link>
+                    <div className="details">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                    />
+                    <hr />
+                    <div className='d-flex justify-content-between align-items-center'>
+                        <div className="price fw-bold">$100</div>
+                        <button className="btn add-to-cart text-dark fw-bold"><i className="fa-solid fa-cart-plus me-2"></i>Add to Cart</button>
                     </div>
                 </div>
+            </div>
         </>
     )
 }
