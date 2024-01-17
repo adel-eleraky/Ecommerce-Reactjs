0a2db9f344b3c8fbce367f4906d71bdbe70513a0
el3raky <kameladel339@gmail.com>
2024-01-17 18:06:56 +0200

    created offcanvas for navigation

---
+++ src/components/NavOffcanvas.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import "./css/NavOffcanvas.css"
+import { Link } from 'react-router-dom'
+
+function NavOffcanvas() {
+    return (
+        <>
+            <div className="nav-offcanvas offcanvas offcanvas-start" tabIndex="-1" id="navOffcanvas" aria-labelledby="navOffcanvas">
+                <div className="offcanvas-header">
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Search Product Here" aria-label="Search Product Here" aria-describedby="basic-addon2" />
+                        <div className="input-group-append">
+                            <span className="input-group-text search-span" id="basic-addon2"><i className="bi bi-search"></i></span>
+                        </div>
+                    </div>
+                    <div className="menu mt-5">
+                        <ul className='p-0'>
+                            <li>
+                                <div className="accordion" id="accordionExample">
+                                    <div className="accordion-item">
+                                        <h2 className="accordion-header">
+                                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                                Shop Categories
+                                            </button>
+                                        </h2>
+                                        <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                                            <div className="accordion-body">
+                                                <h4>gdvfdvfdv</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li><Link to="">Home</Link></li>
+                            <li><Link to="products">Our Store</Link></li>
+                            <li><Link to="blog">Blog</Link></li>
+                            <li><Link to="contact">Contact</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavOffcanvas
