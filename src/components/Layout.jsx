e3266fba820c336c21040f9c13eee63a78a7b687
el3raky <kameladel339@gmail.com>
2024-01-27 22:12:35 +0200

    display loading screen untill fetching products

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import {Outlet} from "react-router-dom"
+import React, { useState } from 'react'
+import { Outlet } from "react-router-dom"
 import Footer from './Footer'
 import Header from './Header';
+import { useDispatch, useSelector } from 'react-redux';
+import HashLoader from "react-spinners/HashLoader";
+import { fetchingProducts } from '../rtk/Slices/ProductSlice';
 
 function Layout() {
+
+	const dispatch = useDispatch()
+
+	const [loading, setLoading] = useState(true)
+
+	React.useEffect(() => {
+
+		dispatch(fetchingProducts()).then(() => setLoading(false))
+	}, [])
+
 	return (
 		<>
-			<Header />
-			<Outlet />
-			<Footer />
+			{loading ?
+				<div className='loading-screen position-absolute top-50 start-50 translate-middle'>
+					<HashLoader
+						color={"#2b273e"}
+						loading={loading}
+						cssOverride={true}
+						size={150}
+						aria-label="Loading Spinner"
+						data-testid="loader"
+					/>
+				</div> :
+				<>
+					<Header />
+					<Outlet />
+					<Footer />
+				</>
+			}
 		</>
 	)
 }
