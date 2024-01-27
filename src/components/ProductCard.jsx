4da453177b751fa463c527c0a2dc816d47a8f09c
el3raky <kameladel339@gmail.com>
2024-01-27 22:15:08 +0200

    display products details from api data

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,9 @@ import ReactStars from "react-rating-stars-component";
 import "./css/ProductCard.css"
 import { Link } from "react-router-dom"
 
-function ProductCard() {
+function ProductCard({ id , title , category , description , price , image , rating}) {
+
+
     return (
         <>
             <div className="featured-product border p-3 mb-3 position-relative rounded" data-aos="fade-up" data-aos-duration="1000">
@@ -15,23 +17,23 @@ function ProductCard() {
                     <img src="/images/img-zoom.svg" className='d-block mb-3' />
                     <img src="/images/add-cart.svg" className='d-block mb-3' />
                 </div>
-                <img className='img-fluid' src="/images/tab.jpg" alt="product img" />
+                <img className='img-fluid product-image mb-3' src={image} alt="product img" />
                 <div className="product-content">
-                    <div className="product-brand mb-3">Havells</div>
+                    <div className="product-category mb-3">{category}</div>
                     <Link to="products/portable-speaker" className='text-dark'>
-                        <div className="product-name fw-bold mb-3">Honor T1 7.0 1GB RAM 8GB ROM</div>
+                        <div className="product-name fw-bold mb-3 text-truncate">{title}</div>
                     </Link>
-                    <div className="details">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div className="details">{description && description.substring(0 , 75) + "...."}</div>
                     <ReactStars
                         count={5}
                         size={24}
-                        value={3}
+                        value={rating && rating.rate}
                         edit={false}
                         activeColor="#ffd700"
                     />
                     <hr />
                     <div className='d-flex justify-content-between align-items-center'>
-                        <div className="price fw-bold">$100</div>
+                        <div className="price fw-bold">${price}</div>
                         <button className="btn add-to-cart text-dark fw-bold"><i className="fa-solid fa-cart-plus me-2"></i>Add to Cart</button>
                     </div>
                 </div>
