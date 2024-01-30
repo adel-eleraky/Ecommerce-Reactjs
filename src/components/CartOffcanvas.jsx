ed0f36082e8906e2cfb8476ab2c353bcf8d39e68
el3raky <kameladel339@gmail.com>
2024-01-30 15:05:46 +0200

    created cart items from cart state & display them in cart offcanvas

--- src/components/CartOffcanvas.jsx
+++ src/components/CartOffcanvas.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import "./css/CartOffcanvas.css"
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeFromCart } from '../rtk/Slices/CartSlice'
 
 function CartOffcanvas() {
+
+    const cart = useSelector(state => state.cart)
+    const dispatch = useDispatch()
+
+    const cartItems = cart && cart.map((item, index) => {
+
+        const { id, title, image, price, quantity } = item
+
+        return (
+            <div key={index} className="cart-item d-flex justify-content-around align-items-center">
+                <i className="bi bi-x-octagon delete-btn fs-5" onClick={() => dispatch(removeFromCart({ id }))}></i>
+                <img src={image} alt="" className="img-fluid" />
+                <div className="content text-center">
+                    <h5 className='title'>{title}</h5>
+                    <span className="price">{quantity} * {price}$</span>
+                </div>
+            </div>
+        )
+    })
     return (
         <>
             <div className="cart-offcanvas offcanvas offcanvas-end" tabIndex="-1" id="cartOffcanvas" aria-labelledby="cartOffcanvas">
@@ -11,18 +32,16 @@ function CartOffcanvas() {
                     <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div className="offcanvas-body">
-                    <div className="cart-item d-flex justify-content-around align-items-center">
-                        <i className="bi bi-x-octagon delete-btn fs-5"></i>
-                        <img src="/images/headphone.jpg" alt="" className="img-fluid" />
-                        <div className="content text-center">
-                            <h5 className='title'>portable speaker</h5>
-                            <span className="price">1 * 50$</span>
-                        </div>
-                    </div>
-                    <hr />
-                    <Link to="cart" className="btn text-white">View Cart</Link>
+                    {cart.length === 0 ?
+                        <div className='empty-cart' >Your Cart is empty</div>
+                        :
+                        <>
+                            {cartItems}
+                            < Link to="cart" className="btn text-white d-block text-center w-50 mt-4 mx-auto">View Cart</Link>
+                        </>
+                    }
                 </div>
-            </div>
+            </div >
         </>
     )
 }
