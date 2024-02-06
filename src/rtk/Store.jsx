4eeec52c16687f0b8fad82dae167e1adeb987aca
el3raky <kameladel339@gmail.com>
2024-02-07 00:16:57 +0200

    added categories reducer to the store

--- src/rtk/Store.jsx
+++ src/rtk/Store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "./Slices/ProductSlice";
-import cartReducer from "./Slices/CartSlice"
+import productsReducer from "./features/ProductSlice";
+import cartReducer from "./features/CartSlice"
+import categoriesReducer from "./features/CategorySlice"
 
 const store = configureStore({
     reducer: {
         products: productsReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        categories: categoriesReducer
     }
 })
 
