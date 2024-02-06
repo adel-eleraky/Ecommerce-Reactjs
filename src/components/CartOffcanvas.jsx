14fe5a080d444d8c3dd217bed188cbc5e0d0774e
el3raky <kameladel339@gmail.com>
2024-02-07 00:01:13 +0200

    added total price for the offcanvas

--- src/components/CartOffcanvas.jsx
+++ src/components/CartOffcanvas.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import "./css/CartOffcanvas.css"
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { removeFromCart } from '../rtk/Slices/CartSlice'
+import { removeFromCart } from '../rtk/features/CartSlice'
 
 function CartOffcanvas() {
 
     const cart = useSelector(state => state.cart)
+
+    const totalPrice = cart && cart.reduce((acc, item) => {
+        return acc + item.price * item.quantity
+    }, 0)
+
     const dispatch = useDispatch()
 
     const cartItems = cart && cart.map((item, index) => {
@@ -17,7 +22,7 @@ function CartOffcanvas() {
             <div key={index} className="cart-item d-flex justify-content-around align-items-center">
                 <i className="bi bi-x-octagon delete-btn fs-5" onClick={() => dispatch(removeFromCart({ id }))}></i>
                 <img src={image} alt="" className="img-fluid" />
-                <div className="content text-center">
+                <div className="content text-center flex-grow-1">
                     <h5 className='title'>{title}</h5>
                     <span className="price">{quantity} * {price}$</span>
                 </div>
@@ -37,6 +42,7 @@ function CartOffcanvas() {
                         :
                         <>
                             {cartItems}
+                            <div className='total-price mt-3 fw-bold'>Total Price: <span className='price'>{totalPrice}$</span></div>
                             < Link to="cart" className="btn text-white d-block text-center w-50 mt-4 mx-auto">View Cart</Link>
                         </>
                     }
