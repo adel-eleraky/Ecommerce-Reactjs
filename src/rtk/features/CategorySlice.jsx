6e6460d90037d1f5d46fb280d3e1b4f5a6eedb31
el3raky <kameladel339@gmail.com>
2024-02-06 23:58:13 +0200

    changed the folder name to features && create categories slice

---
+++ src/rtk/features/CategorySlice.jsx
@@ -0,0 +1,29 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchingCategories = createAsyncThunk(
+    "categories/fetchingCategories",
+    async () => {
+
+        const options = {
+            method: "get",
+            url: "https://fakestoreapi.com/products/categories"
+        }
+        const result = await axios(options)
+        return result.data
+    }
+)
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState: [],
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchingCategories.fulfilled , (state , action) => {
+            return action.payload
+        })
+    }
+})
+
+export  const { } = categoriesSlice.actions
+export default categoriesSlice.reducer
