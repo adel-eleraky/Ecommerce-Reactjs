619ffbdc92a8c33d77349534c3c496a172910cfd
el3raky <kameladel339@gmail.com>
2024-02-07 00:09:56 +0200

    added function to handle search params && created react-stars-elements

--- src/components/StoreSidebar.jsx
+++ src/components/StoreSidebar.jsx
@@ -1,55 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/StoreSidebar.css"
 import ReactStars from 'react-rating-stars-component';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 function StoreSidebar() {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const handleSearchParams = (name, value) => {
+
+        const newSearchParams = new URLSearchParams(searchParams.toString())
+        newSearchParams.set(name, value)
+        setSearchParams(newSearchParams)
+    }
+    
+    const reactStarElements = []
+    for (let i = 1; i <= 5; i++) {
+
+        reactStarElements.push(
+            <div key={i} className='rating-element' onClick={() => handleSearchParams("rating", i)}>
+                <ReactStars count={5} size={24} value={i} edit={false} activeColor="#ffd700" />
+            </div>
+        )
+    }
+
     return (
         <>
             <div className="sidebar">
                 <div className="sidebar-filter  bg-white rounded p-3 mb-3">
                     <h5 className='mb-4'>Filter By</h5>
-                    <div className="Availability py-3 ms-2 mb-3 border-top border-3">
-                        <h6 className='mb-3'>Availability</h6>
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-                            <label className="form-check-label" htmlFor="flexCheckDefault">
-                                Default checkbox
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
-                            <label className="form-check-label" htmlFor="flexCheckChecked">
-                                Checked checkbox
-                            </label>
-                        </div>
-                    </div>
                     <div className="price py-3 ms-2 mb-3 border-top border-3">
                         <h6 className='mb-3'>Price</h6>
                         <div className="inputs d-flex justify-content-between gap-15">
-                            <input type="number" className="form-control" placeholder="From" />
-                            <input type="number" className="form-control" placeholder="To" />
-                        </div>
-                    </div>
-                    <div className="brand py-3 ms-2 mb-3 border-top border-3">
-                        <h6 className='mb-3'>Product Brand</h6>
-                        <div className="brands d-flex flex-wrap gap-15">
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>Apple</span>
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>Redragon</span>
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>Razer</span>
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>COSRX</span>
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>SHARPIE</span>
-                            <span className='bg-secondary-subtle fw-normal py-1 px-2 rounded'>Apple</span>
+                            <input type="number" name="priceFrom" className="form-control" placeholder="From" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
+                            <input type="number" name="priceTo" className="form-control" placeholder="To" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
                         </div>
                     </div>
                     <div className="rating py-3 ms-2 mb-3 border-top border-3">
                         <h6 className='mb-3'>Rating</h6>
                         <div className="ratings">
-                            <ReactStars count={5} size={24} value={1} edit={false} activeColor="#ffd700" />
-                            <ReactStars count={5} size={24} value={2} edit={false} activeColor="#ffd700" />
-                            <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />
-                            <ReactStars count={5} size={24} value={4} edit={false} activeColor="#ffd700" />
-                            <ReactStars count={5} size={24} value={5} edit={false} activeColor="#ffd700" />
+                            {reactStarElements}
                         </div>
                     </div>
                 </div>
