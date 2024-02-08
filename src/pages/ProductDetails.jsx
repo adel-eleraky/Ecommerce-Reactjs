a437dfcc048b6c51dd99429db51e69951f7a7965
el3raky <kameladel339@gmail.com>
2024-02-09 01:52:36 +0200

    added func for add-to-cart && func for add-to-wishlist

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,53 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/ProductDetails.css"
 import { useParams } from 'react-router-dom'
 import ReactStars from "react-rating-stars-component";
 import ProductSwiper from '../components/ProductSwiper';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../rtk/features/CartSlice';
+import { addToWishlist } from '../rtk/features/WishlistSlice';
 
 
 function ProductDetails() {
 
-    const {id} = useParams()
+    const { productId } = useParams()
+    const dispatch = useDispatch()
+    const [quantity, setQuantity] = useState(1)
 
     const products = useSelector(state => state.products)
-    const productDetails = products.filter(product => product.id === +id)[0]
+    const productDetails = products.filter(product => product.id === +productId)[0]
 
+    const { id, title, category, description, price, image, rating } = productDetails
     return (
         <>
             <div className="product-page py-5">
                 <div className="container">
                     <div className="row">
                         <div className="col-12 col-md-6 mb-5 mb-md-0">
-                            <ProductSwiper images={[productDetails.image]} />
+                            <ProductSwiper images={[image]} />
                         </div>
                         <div className="col-12 col-md-6 ">
                             <div className="content">
-                                <h3 className="name border-bottom pb-2">{productDetails.title}</h3>
-                                <h4 className="price">Price: {productDetails.price}$</h4>
-                                <h4 className="rating">Rating: {productDetails.rating.rate}
+                                <h3 className="name border-bottom pb-2">{title}</h3>
+                                <h4 className="price">Price: {price}$</h4>
+                                <h4 className="rating">Rating: {rating.rate}
                                     <ReactStars
                                         count={5}
                                         size={24}
-                                        value={productDetails.rating.rate}
+                                        value={rating.rate}
                                         edit={true}
                                         activeColor="#ffd700"
                                     />
                                 </h4>
                                 <hr />
                                 <div className="product-details">
-                                    <h4 className='category'>Category: <span className='fw-light'>{productDetails.category}</span></h4>
+                                    <h4 className='category'>Category: <span className='fw-light'>{category}</span></h4>
                                     <h4 className='availability'>Availability: <span className='fw-light'>5 In-Stock</span></h4>
-                                    <h4 className='quantity'>Quantity: <input type="number" name="quantity" defaultValue="1" /></h4>
+                                    <h4 className='quantity'>Order Quantity: <input type="number" name="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} /></h4>
                                 </div>
-                                <button className="btn add-to-cart text-dark fw-bold me-3"><i className="fa-solid fa-cart-plus me-2"></i>Add to Cart</button>
-                                <button className="btn add-to-wishlist text-dark fw-bold"><i className="fa-regular fa-heart me-2"></i>Add to WishList</button>
+                                <button className="btn add-to-cart text-dark fw-bold me-3" onClick={() => dispatch(addToCart({ id, title, image, price, quantity }))}><i className="fa-solid fa-cart-plus me-2"></i>Add to Cart</button>
+                                <button className="btn add-to-wishlist text-dark fw-bold" onClick={() => dispatch(addToWishlist({ id, title, category, description, price, image, rating }))}><i className="fa-regular fa-heart me-2"></i>Add to WishList</button>
                             </div>
                         </div>
                     </div>
                     <div className="description">
                         <h3>Description</h3>
-                        <p>{productDetails.description}</p>
+                        <p>{description}</p>
                     </div>
                 </div>
             </div>
