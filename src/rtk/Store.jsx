de93c85a8739ed633b19f0ce3bcc5444aca6eb53
el3raky <kameladel339@gmail.com>
2024-02-09 01:57:56 +0200

    added wishlist reducer to the store

--- src/rtk/Store.jsx
+++ src/rtk/Store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./features/ProductSlice";
 import cartReducer from "./features/CartSlice"
 import categoriesReducer from "./features/CategorySlice"
+import wishlistReducer from "./features/WishlistSlice";
 
 const store = configureStore({
     reducer: {
         products: productsReducer,
         cart: cartReducer,
-        categories: categoriesReducer
+        categories: categoriesReducer,
+        wishlist: wishlistReducer
     }
 })
 
