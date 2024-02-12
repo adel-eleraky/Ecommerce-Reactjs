4c22f626bd31e4d026898a3476ed5a530308d5a6
el3raky <kameladel339@gmail.com>
2024-02-13 01:15:18 +0200

    remove the console.log statement

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./css/Wishlist.css"
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import ProductCard from '../components/ProductCard'
@@ -10,13 +9,11 @@ function Wishlist() {
     const wishlistItems = wishlist && wishlist.map(product => {
         return (
             <div key={product.id} className="col-12 col-sm-6 col-md-4 col-lg-3">
-                <ProductCard {...product } wished="true" />
+                <ProductCard {...product } />
             </div>
         )
     })
 
-    console.log(wishlist)
-    console.log(wishlistItems)
     return (
         <div className="wishlist-page py-4">
             {wishlist && wishlist.length > 0 ?
