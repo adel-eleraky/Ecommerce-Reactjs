8fc4b5d6125ac31fa723088421c2f87708150e16
el3raky <kameladel339@gmail.com>
2024-02-13 01:10:04 +0200

    re-design price filter

--- src/components/StoreSidebar.jsx
+++ src/components/StoreSidebar.jsx
@@ -32,8 +32,8 @@ function StoreSidebar() {
                     <h5 className='mb-4'>Filter By</h5>
                     <div className="price py-3 ms-2 mb-3 border-top border-3">
                         <h6 className='mb-3'>Price</h6>
-                        <div className="inputs d-flex justify-content-between gap-15">
-                            <input type="number" name="priceFrom" className="form-control" placeholder="From" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
+                        <div className="inputs">
+                            <input type="number" name="priceFrom" className="form-control mb-2" placeholder="From" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
                             <input type="number" name="priceTo" className="form-control" placeholder="To" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
                         </div>
                     </div>
