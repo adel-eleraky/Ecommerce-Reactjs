768fbac13cf5723a98ea402e3073f9dcc50fa58a
el3raky <kameladel339@gmail.com>
2024-02-13 01:04:56 +0200

    added notify toast

--- src/components/CartOffcanvas.jsx
+++ src/components/CartOffcanvas.jsx
@@ -3,6 +3,8 @@ import "./css/CartOffcanvas.css"
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { removeFromCart } from '../rtk/features/CartSlice'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function CartOffcanvas() {
 
@@ -12,16 +14,34 @@ function CartOffcanvas() {
         return acc + item.price * item.quantity
     }, 0)
 
+    const notify = () => {
+        toast.error('Product Deleted from Cart', {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+        });
+    }
+
     const dispatch = useDispatch()
+    const handleDelete = (id) => {
+
+        dispatch(removeFromCart({ id }))
+        notify()
+    }
 
     const cartItems = cart && cart.map((item, index) => {
 
-        const { id, title, image, price, quantity } = item
+        const { id, title, thumbnail, price, quantity } = item
 
         return (
             <div key={index} className="cart-item d-flex justify-content-around align-items-center">
-                <i className="bi bi-x-octagon delete-btn fs-5" onClick={() => dispatch(removeFromCart({ id }))}></i>
-                <img src={image} alt="" className="img-fluid" />
+                <i className="bi bi-x-octagon delete-btn fs-5" onClick={() => handleDelete(id)}></i>
+                <img src={thumbnail} alt="" className="img-fluid" />
                 <div className="content text-center flex-grow-1">
                     <h5 className='title'>{title}</h5>
                     <span className="price">{quantity} * {price}$</span>
@@ -29,6 +49,7 @@ function CartOffcanvas() {
             </div>
         )
     })
+
     return (
         <>
             <div className="cart-offcanvas offcanvas offcanvas-end" tabIndex="-1" id="cartOffcanvas" aria-labelledby="cartOffcanvas">
