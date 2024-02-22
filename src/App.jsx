0589b99145029fbcd65573cf613320afbb06f191
el3raky <kameladel339@gmail.com>
2024-02-22 23:52:01 +0200

    removed blog route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
-import Blog from "./pages/Blog"
 import Login from "./pages/Login"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -31,7 +30,6 @@ function App() {
                             <Route index element={<Home />} />
                             <Route path="products/:category?" element={<Store />} />
                             <Route path="product/:productId" element={<ProductDetails />} />
-                            <Route path="blog" element={<Blog />} />
                             <Route path="login" element={<Login />} />
                             <Route path="sign-up" element={<Signup />} />
                             <Route path="cart" element={<Cart />} />
