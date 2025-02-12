b00a1a987ab050bcc5dcea45e43c9b3f77259fa5
el3raky <kameladel339@gmail.com>
2025-02-12 13:15:06 +0200

    added categories to the filters

--- src/components/StoreSidebar.jsx
+++ src/components/StoreSidebar.jsx
@@ -2,19 +2,29 @@
 import React, { useState } from 'react'
 import "./css/StoreSidebar.css"
 import ReactStars from 'react-rating-stars-component';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+import { useSelector } from 'react-redux';
 
 function StoreSidebar() {
 
     const [searchParams, setSearchParams] = useSearchParams()
+    const categories = useSelector(state => state.categories.data)
 
+    console.log(categories)
     const handleSearchParams = (name, value) => {
 
         const newSearchParams = new URLSearchParams(searchParams.toString())
         newSearchParams.set(name, value)
         setSearchParams(newSearchParams)
     }
-    
+
     const reactStarElements = []
     for (let i = 1; i <= 5; i++) {
 
@@ -28,13 +38,31 @@ function StoreSidebar() {
     return (
         <>
             <div className="sidebar">
-                <div className="sidebar-filter  bg-white rounded p-3 mb-3">
+                <div className="sidebar-filter bg-white rounded p-3 mb-3">
                     <h5 className='mb-4'>Filter By</h5>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            <Typography component="span" className='fw-bold'>Categories</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            {categories && categories.map((category, index) => {
+                                return (
+                                    <p className="category-item mb-0" key={index}>
+                                        <Link to={`${category.slug}`} className="d-block text-dark p-2">{category.name}</Link>
+                                    </p>
+                                )
+                            })}
+                        </AccordionDetails>
+                    </Accordion>
                     <div className="price py-3 ms-2 mb-3 border-top border-3">
                         <h6 className='mb-3'>Price</h6>
                         <div className="inputs">
-                            <input type="number" name="priceFrom" className="form-control mb-2" placeholder="From" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
-                            <input type="number" name="priceTo" className="form-control" placeholder="To" onChange={(e) => handleSearchParams(e.target.name , e.target.value)} />
+                            <input type="number" name="priceFrom" className="form-control mb-2" placeholder="From" onChange={(e) => handleSearchParams(e.target.name, e.target.value)} />
+                            <input type="number" name="priceTo" className="form-control" placeholder="To" onChange={(e) => handleSearchParams(e.target.name, e.target.value)} />
                         </div>
                     </div>
                     <div className="rating py-3 ms-2 mb-3 border-top border-3">
